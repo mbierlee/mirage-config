525e82410f0e22a6fcfb012586d786d204a17511
Mike Bierlee <m.bierlee@lostmoment.com>
2022-09-23 23:34:22 +0300

    Add project skeleton

---
+++ source/poodinis/config/package.d
@@ -0,0 +1,10 @@
+/**
+ * Authors:
+ *  Mike Bierlee, m.bierlee@lostmoment.com
+ * Copyright: 2022 Mike Bierlee
+ * License:
+ *  This software is licensed under the terms of the MIT license.
+ *  The full terms of the license can be found in the LICENSE file.
+ */
+
+module poodinis.config;
